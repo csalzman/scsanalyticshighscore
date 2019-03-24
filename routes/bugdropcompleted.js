0be990dd0ea1778d336c53893b376a0bfe65011a
Christopher Salzman <christopher.salzman@gmail.com>
2019-03-24 10:14:25 -0400

    moving routes to their own file

---
+++ routes/bugdropcompleted.js
@@ -0,0 +1,64 @@
+//Bug Drop Completed Routes
+const express = require('express');
+const router = express.Router();
+
+//bodyParser middleware so we can quickly get the body of the request
+const bodyParser = require('body-parser');
+router.use(bodyParser.json());
+
+//Database setup
+const mysql = require('mysql');
+const db = require('../db.js');
+const dbConnection = mysql.createConnection(db.dbSettings);
+
+dbConnection.connect((err) => {
+	if(err) {
+		throw err;
+	}
+	console.log('connected');
+});
+
+/* Testing Curl request: 
+curl -X POST http://localhost:8080/bugdrop/completed -H "Content-type: application/json" -d '{"playerID": "123123","levelName":"Woo", "completionTime":"10.5", "collectables":"5", "attemptNum":"3"}'
+*/
+//Posts row 
+router.post('/', function (req, res) { 
+	console.log("Post request from: " + req.ip);
+
+	const requestJson = req.body;
+	//Check that the json request contains what we think it should
+	if(
+		requestJson.playerID != null &&
+		requestJson.levelName != null &&
+		requestJson.completionTime != null &&
+		requestJson.collectables != null &&
+		requestJson.attemptNum != null
+	) {
+		dbConnection.query(
+			'INSERT INTO level_test SET playerID = ?, completionTime =  ?, collectables = ?, attemptNum = ?',
+			[requestJson.playerID, requestJson.completionTime, requestJson.collectables, requestJson.attemptNum],
+			function (error, results, fields) {
+				if(error) throw error;
+				
+				console.log("Added with ID: " + results.insertId);
+			}
+		 );
+		
+		res.send("The server would like to thank you.\nResponse sent: " + new Date());
+	}	
+	else {
+		res.send("Problem with your request. Missing fields");
+	}
+});
+
+//Display our data
+router.get('/', function (req, res) { 
+	dbConnection.query('SELECT * FROM level_test', function (err, result, fields) {
+		if(err) {
+			throw err;
+		}
+		res.send(result);
+	});
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const mysql = require('mysql');
-const db = require('./db.js');
+const router = express.Router();
 
 //bodyParser middleware so we can quickly get the body of the request
 const bodyParser = require('body-parser');
@@ -8,55 +7,8 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 8080;
 
-const dbConnection = mysql.createConnection(db.dbSettings);
+var bugdropcompleted = require('./routes/bugdropcompleted.js');
 
-dbConnection.connect((err) => {
-	if(err) {
-		throw err;
-	}
-	console.log('connected');
-});
-
-app.get('/bugdrop/completed', function (req, res) { 
-	var woo = dbConnection.query('SELECT * FROM level_test', function (err, result, fields) {
-		if(err) {
-			throw err;
-		}
-		res.send(result);
-	});
-});
-
-app.use(bodyParser.json());
-
-//Testing Curl request
-//curl -X POST http://localhost:8080/bugdrop/completed -H "Content-type: application/json" -d '{"playerID": "123123","levelName":"Woo", "completionTime":"10.5", "collectables":"5", "attemptNum":"3"}'
-app.post('/bugdrop/completed', function (req, res) { 
-	console.log("Post request from: " + req.ip);
-
-	const requestJson = req.body;
-	//Check that the json request contains what we think it should
-	if(
-		requestJson.playerID != null &&
-		requestJson.levelName != null &&
-		requestJson.completionTime != null &&
-		requestJson.collectables != null &&
-		requestJson.attemptNum != null
-	) {
-		dbConnection.query(
-			'INSERT INTO level_test SET playerID = ?, completionTime =  ?, collectables = ?, attemptNum = ?',
-			[requestJson.playerID, requestJson.completionTime, requestJson.collectables, requestJson.attemptNum],
-			function (error, results, fields) {
-				if(error) throw error;
-				
-				console.log("Added with ID: " + results.insertId);
-			}
-		 );
-		
-		res.send("The server would like to thank you.\nResponse sent: " + new Date());
-	}	
-	else {
-		res.send("Problem with your request. Missing fields");
-	}
-});
+app.use('/bugdrop/completed', bugdropcompleted);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
